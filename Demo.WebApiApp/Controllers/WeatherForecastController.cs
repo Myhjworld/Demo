da41c464eaefcd07b4e595c23e03495b1789a4c1
Elijah Jian <hjxtty@outlook.com>
2020-04-01 14:50:37 +0800

    dotnetCoreVSCodePrj

---
+++ Demo.WebApiApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.WebApiApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        // [HttpGet]
+        // public IEnumerable<WeatherForecast> GetE()
+        // {
+        //     var rng = new Random();
+        //     var Result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        //     _logger.LogDebug(Result.ToString());
+        //     return Result;
+        // }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "yes";
+        }
+
+    }
+}
